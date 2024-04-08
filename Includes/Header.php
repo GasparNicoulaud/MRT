de26d9e7afba146c37e58be8999a79015bf4db25
GasparNicoulaud <g.nicoulaud@gmail.com>
2024-04-08 16:45:16 +0200

    tets

--- Includes/Header.php
+++ Includes/Header.php
@@ -4,36 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mozziconacci Robert-Teyssier</title>
+    <title>mozziconacci robert-teyssier</title>
     <link rel="stylesheet" href="/assets/style.css">
   </head>
   <body>
     <header>
         <style>
             header{
+                font-size: 25px;
+
                 > div {
                     display: flex;
                     justify-content: space-between;
                     align-items: center;
                     padding: var(--margin-size);
+                    border-bottom: 2px solid var(--main);
 
-                    > a.gauche {
+                    > a.gauche h1 {
                         font-family: 'picnic', serif;
+                    }
+                }
+
+                > nav {
+                    display: flex;
+                    justify-content: space-around;
+                    padding: calc(var(--margin-size)/2) var(--margin-size);
+                    border-bottom: 2px solid var(--main);
+
+                    > a {
+                        text-decoration: none;
+                        color: var(--main);
                     }
                 }
             }
         </style>
         <div>
             <a class="gauche" href="/"><h1>MRT</h1></a>
-            <a class="droite" href="/"><h1>Mozziconacci Robert-Teyssier</h1></a>
+            <a class="droite" href="/"><h1>mozziconacci robert-teyssier</h1></a>
         </div>
         <nav>
-            <ul>
-                <li><a href="texte.php">texte</a></li>
-                <li><a href="video.php">vidéo</a></li>
-                <li><a href="publication.php">publication</a></li>
-                <li><a href="#">portfolio</a></li>
-                <li><a href="biographie.php">biographie</a></li>
-            </ul>
+            <a href="texte.php">texte</a>
+            <a href="video.php">vidéo</a>
+            <a href="publication.php">publication</a>
+            <a href="#">portfolio</a>
+            <a href="biographie.php">biographie</a>
         </nav>
     </header>
